97a7773c2bd24f8c5d0c673b1585277ee794ccf6
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-19 18:46:51 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,26 @@ st.sidebar.caption("Que desea hacer?")
 st.sidebar.write("\n")
 st.sidebar.selectbox("Seleccione una opcion:", ['Consultar tablas', 'Apartado de Asistencias'])
 
-st.sidebar.title("Menu")
-opciones=["Consultar tablas", "Apartado de Asistencias"]
-
-st.sidebar.seleccion_menu = option_menu(
+with st.sidebar:
+        beta_sign = """
+        <span style="
+        font-size: 10px;
+        font-weight: bold;
+        color: #ffffff;
+        background-color: #ff5733;
+        padding: 5px 10px;
+        border-radius: 4px;
+        ">
+            BETA
+        </span>
+        """
+        seleccion_menu = option_menu(
             menu_title="Menú",
             options=[
                 "Autenticarse", 
                 "Registrarse",
                 "Personalizar",
                 "Distribuir",                
-            ]
+                "Visualizar", #TODO visualiza stats y gráficos de correos enviados, hacer informes ? fechas etc, Visualizar el log aqui?
+            ],
             )

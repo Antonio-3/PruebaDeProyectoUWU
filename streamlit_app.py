02fa866fea1474049d6e2e783b05e7c47b5d84ab
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-19 20:14:47 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import ConsultarTablas
+
 
 st.sidebar.image(image='img/LogoPerla.png',caption="")
 st.sidebar.caption("Bienvenido Admin!.")

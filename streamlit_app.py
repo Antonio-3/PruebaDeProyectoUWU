2cc19a6324c6ea359fee80dffd452ba09aba5e02
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-19 19:22:37 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,42 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import ConsultarTablas, Asistencias
 
 
 st.sidebar.image(image='img/imagenProyecto.PNG',caption="")
 st.sidebar.caption("Bienvenido Admin!.")
 
-with st.sidebar:
-        beta_sign = """
-        <span style="
-        font-size: 10px;
-        font-weight: bold;
-        color: #ffffff;
-        background-color: #ff5733;
-        padding: 5px 10px;
-        border-radius: 4px;
-        ">
-            BETA
-        </span>
-        """
-        seleccion_menu = option_menu(
-            menu_title="Menú",
-            options=[
-                "Consultar tablas", 
-                "Asistencias",
-            ],
-            )
+clas MultiApplicaciones:
+        def _init_(self):
+            self.apps= []
+        def add_app(self, title, function):
+            self.apps.append({
+                "title": title
+                "function": function
+            })
+
+        def correr():
+            
+                with st.sidebar:
+                        beta_sign = """
+                        <span style="
+                        font-size: 10px;
+                        font-weight: bold;
+                        color: #ffffff;
+                        background-color: #ff5733;
+                        padding: 5px 10px;
+                        border-radius: 4px;
+                        ">
+                            BETA
+                        </span>
+                        """
+                        seleccion_menu = option_menu(
+                            menu_title="Menú",
+                            options=[
+                                "Consultar tablas", 
+                                "Asistencias",
+                            ],
+                            )
 st.sidebar.write("\n")
 st.sidebar.button("Generar Reportes")
         

3c927f9dc5f31a835f90fff623218b0964848e07
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-22 17:08:29 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ if seleccion_menu == "Generar Reportes":
         pr = st.button("Generar reporte del profesor")
         if pr==True:
                 cursor.execute('''
-                SELECT * FROM materiaprofe WHERE Profesor = seleccion_profeexd['Profesor']
+                SELECT * FROM materiaprofe WHERE Profesor = 'seleccion_profeexd['Profesor']'
                 ''')
                 st.write("\nLista de Proferores:\n")
                 st.write("{:<5} {:<25} {:<20} {:<10} {:20} {:<25} {:<20}".format('ID', 'Profesor', 'Materia', 'Carrera','Fecha','Horario','Asistencia'))

2a250f661bd045c01d7f6d8d6b08bbcaf4758327
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-22 17:31:02 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ if seleccion_menu == "Generar Reportes":
                 # Seleccionar todas las materias
                 cursorr.execute('''
                 SELECT * FROM materiaprofe WHERE Profesor = xddd
-                ''', (seleccion_profeexd,))
+                ''', (seleccion_profeexd))
                 # Recuperar todos los registros
                 jsjsjs = cursorr.fetchall()
                 # Mostrar los registros de forma estructurada

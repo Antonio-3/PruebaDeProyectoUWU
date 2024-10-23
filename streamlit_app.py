b1e9c413c9a7809b5ae96491694bfe3453d3230a
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-22 19:16:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,7 @@ if seleccion_menu == "Generar Reportes":
 
         pr = st.button("Generar reporte del profesor")
         if pr==True:
-
-                st.write(pr)
+                st.write(seleccon_profeexd)
                 # Conectar a la base de datos
                 conexion = sqlite3.connect('BasePrueba/ProfesoresPrueba.db')
                 cursor = conexion.cursor()
